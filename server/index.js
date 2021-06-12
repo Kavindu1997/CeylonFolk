e834f3ceaf4337149a23c7c1b0aa870252d533be
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-06-12 22:03:51 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mysql from 'mysql';
 import cors from 'cors';
-
+import {db} from './config/db.js'
 import Routes from './routes/route.js';
 
 const app=express();
@@ -13,5 +12,7 @@ app.use(cors());
 
 app.use('/index',Routes);
 
+
+
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log(`${PORT}`));
+app.listen(PORT,console.log(`Server running on port: ${PORT}`));
