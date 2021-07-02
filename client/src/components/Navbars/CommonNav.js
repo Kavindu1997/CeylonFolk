27e53d63cdc7461713450d04ee9d37b596580ec0
Thashwini <thashwiniwattuhewa@gmail.com>
2021-07-02 13:08:15 +0530

    committed

--- client/src/components/Navbars/CommonNav.js
+++ client/src/components/Navbars/CommonNav.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { AppBar,Typography,Button,IconButton,Toolbar} from '@material-ui/core';
+import { AppBar,Typography,Button,IconButton,CardMedia,Toolbar} from '@material-ui/core';
 import LocalMallSharpIcon from '@material-ui/icons/LocalMallSharp';
 import { makeStyles } from '@material-ui/styles';
+// import logo from '../images/cf_logo_long.png';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 
@@ -24,6 +26,10 @@ const useStyles=makeStyles((theme)=>({
         flexGrow:'1',
         color:'#fff',
     },
+    appbarTitle2:{
+        flexGrow:'2',
+        color:'#fff',
+    },
     appbarWrapper:{
         width:'100%',
         margin:'0 auto',
@@ -38,6 +44,10 @@ const useStyles=makeStyles((theme)=>({
         textTransform: 'none',
         fontSize:'15px'
     },
+    goDown:{
+        color:'#fff',
+        fontSize:'1rem',
+    },
   
  }))
 
@@ -50,11 +60,23 @@ const CommonNav = () => {
                <Toolbar className={classes.appbarWrapper}>
                    <div className={classes.appbarTitle}>
                     <Button color="white" href="/index"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'uppercase',fontSize:'15px',fontWeight:'100'}}>Home</Button>
-                    <Button color="white"  href="/shop"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'uppercase',fontSize:'15px',fontWeight:'100'}}>Shop</Button>
+                    <Button color="white"  href="/shop"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'uppercase',fontSize:'15px',fontWeight:'100'}}>Shop<IconButton>
+                        <ExpandMoreIcon className={classes.goDown}/>
+                    </IconButton>
+                    </Button>
                     <Button color="white" href="/contactus"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'uppercase',fontSize:'15px',fontWeight:'100'}}>Contact</Button>
                     <Button color="white" href="/contactus"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'uppercase',fontSize:'15px',fontWeight:'100'}}>About us</Button>
                     <Button color="white" href="/login"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'uppercase',fontSize:'15px',fontWeight:'100'}}>Login</Button>
                    </div> 
+                   {/* <div className={classes.appbarTitle2}>
+                   <CardMedia
+                                        className={classes.media}
+                                        style={{ logo:`url(${cf_logo_long})`}}
+                                        />
+
+                   </div> */}
+                   
+
                    {/* <div className={classes.navbartext}>
                     <Button color="white" href="/index"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'none',fontSize:'15px',fontWeight:'100'}}>Home</Button>
                     <Button color="white"  href="/shop"  style={{color:'white',fontFamily:'Segoe UI',textTransform: 'none',fontSize:'15px',fontWeight:'100'}}>Shop</Button>
