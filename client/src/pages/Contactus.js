b50834c4c56613f7e156f3c7a1c7720477ffe338
Pramuka Seneviratne <41991286+PramukaS@users.noreply.github.com>
2021-07-04 18:46:38 +0530

    Update Contactus.js

--- client/src/pages/Contactus.js
+++ client/src/pages/Contactus.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ContactUs from '../components/Contactus/ContactUs';
+import CommonNav from '../components/Navbars/CommonNav';
+import Footer from '../components/Footer/Footer';
 const Contactus = () => {
     return (
         <div>
+            <CommonNav/>
             <ContactUs/>
+            <Footer/>
         </div>
     );
 };
