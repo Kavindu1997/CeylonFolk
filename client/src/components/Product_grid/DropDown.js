372f6886ca8d1c0704bdea6ddec6b48db4d15cf3
Jananigamage <Starkrizy.jani@gmail.com>
2021-07-10 12:37:12 +0530

    Previous_dropDownMenu

--- client/src/components/Product_grid/DropDown.js
+++ client/src/components/Product_grid/DropDown.js
@@ -1,72 +1,164 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import { Grid } from '@material-ui/core';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
+import {Typography,IconButton,Collapse,Box,Container,Card,CardActionArea,CardActions,CardContent,CardMedia} from '@material-ui/core';
+
+const options = ['Collection','Snowy', 'Marvel'];
+
+
+const useStyles=makeStyles((theme)=>({
+  root:{
+      display:'flex',
+      justifyContent:'left',
+      alignItems:'center',
+      height:'1000px',
+      fontFamily:'Segoe UI' ,
+      backgroundImage:`linear-gradient(rgba(0,0,0,0.1),rgba(0,0,0,0.1)),url(${Image})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      position:'relative',
+      padding: '100px',
+      color: 'white'
+    
   },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
+  backimage: {
+      marginTop: '60px'
+
   },
+  colorText:{
+      color:'#31C5EE',
+      fontSize:'4rem',
+  },
+  title:{
+   color:'white',
+   fontSize:'3rem',
+   textAlign: 'left',
+   fontFamily:'Segoe UI',
+  },
+  container:{
+      textAlign:'left'
+  },
+  goDown:{
+      color:'#fff',
+      fontSize:'4rem'
+  },
+  collectionContainer:{
+      paddingTop:'24px'
+  },
+  collectionTitle:{
+      marginTop: '30px',
+      fontWeight:'300',
+      paddingBottom:'24px',
+      textAlign:'center',
+      fontFamily:'Segoe UI',
+      padding: '50px',
+  },
+  card:{
+      maxWidth:'95%'
+      
+  },
+  media:{
+      height:'240px',
+     
+  }
+
 }));
 
-export const DropDown = () => {
-  const classes = useStyles();
-  const [age, setAge] = React.useState('');
-  const [open, setOpen] = React.useState(false);
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+export const DropDown = () => {
+  const classes=useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
   };
 
-  const handleClose = () => {
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
     setOpen(false);
   };
 
-  const handleOpen = () => {
-    setOpen(true);
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
   };
 
   return (
+      <div>
+
+    <Typography variant="h4" className={classes.collectionTitle}>WORK WEAR</Typography>
+            <Grid container direction="column" alignItems="center">
+      <Grid item xs={12}>
+        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+          <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+          <Button
+            color="primary"
+            size="small"
+            aria-controls={open ? 'split-button-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-label="select merge strategy"
+            aria-haspopup="menu"
+            onClick={handleToggle}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+        </ButtonGroup>
 
 
-<div>
-      <Button className={classes.button} onClick={handleOpen}>
-        Open the select
-      </Button>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-controlled-open-select-label">Collection</InputLabel>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Snowy</MenuItem>
-          <MenuItem value={20}>Marvel</MenuItem>
-          <MenuItem value={30}>BTS</MenuItem>
-        </Select>
-      </FormControl>
 
+
+
+
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList id="split-button-menu">
+                    {options.map((option, index) => (
+                      <MenuItem
+                        key={option}
+                        disabled={index === 2}
+                        selected={index === selectedIndex}
+                        onClick={(event) => handleMenuItemClick(event, index)}
+                      >
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Grid>
+    </Grid>
     </div>
-
-
-
-  );
-
   
+  );
 }
