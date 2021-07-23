60ae5e26abfd219b8c550ec858ff399e0bf2573f
Jananigamage <Starkrizy.jani@gmail.com>
2021-07-23 16:25:32 +0530

    Inventory_retrieved

--- server/routes/Inventory.js
+++ server/routes/Inventory.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { Inventory } = require('../models/');
 
+router.get("/inventory", async (req,res) => {
+    const listOfItems = await Inventory.findAll();
+    res.json(listOfItems);
+});
+
 router.post("/inventory", (req, res) => {
     const { code, colour, size, type, quantity, margin } = req.body;
         // console.log(fullName)
