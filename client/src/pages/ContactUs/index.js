e545bc033f821f578f78eb91d8b3b63c091db990
Jananigamage <Starkrizy.jani@gmail.com>
2021-08-09 00:28:00 +0530

    Added Styles.js

--- client/src/pages/ContactUs/index.js
+++ client/src/pages/ContactUs/index.js
@@ -12,7 +12,7 @@ import * as Yup from 'yup';
 import "yup-phone";
 import Select from '../../components/FormsUI/Select';
 import types from '../../components/Data/types.json';
-
+import useStyles from './style';
 
 const initialValues = {
   fullName: '',
@@ -42,58 +42,6 @@ const onSubmit = (data, props) => {
   props.resetForm();
 };
 
-const useStyles = makeStyles((theme) => ({
-  infoContent: {
-    backgroundColor: '#f5f6fa',
-    textAlign: 'center',
-    padding: theme.spacing(5),
-    '& .MuiTypography-h5': {
-      fontFamily: 'Segoe UI',
-    },
-    '& .MuiIconButton-root': {
-      color: '#1e272e',
-    },
-    '& .MuiTypography-subtitle1': {
-      fontFamily: 'Nunito',
-      color: '#596275'
-    },
-  },
-  headStyle: {
-    fontFamily: 'Nunito',
-    color: '#222f3e',
-    textAlign: 'center',
-    fontSize: '2.5rem',
-    '&:hover': {
-      color: '#48dbfb',
-    }
-  },
-  textStyle: {
-    fontFamily: 'Segoe UI',
-    color: '#2C2D2D',
-    paddigBottom: '20px'
-  },
-  pageContent: {
-    margin: theme.spacing(5),
-    padding: theme.spacing(3),
-  },
-  formControl: {
-    marginTop: theme.spacing(1),
-    minWidth: 200,
-  },
-  selectEmpty: {
-    margin: theme.spacing(2),
-  },
-  submit: {
-    align: 'center',
-    padding: '10px',
-    marginTop: '20px',
-  },
-  textField: {
-    '& p': {
-      color: 'red'
-    }
-  },
-}));
 
 export default function ContactUs() {
   const classes = useStyles();
