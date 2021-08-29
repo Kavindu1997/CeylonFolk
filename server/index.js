7f30f3bd032d5bb4c8727540b4b9d1b2a010d316
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-08-29 21:16:22 +0530

    Revert "changed"
    This reverts commit adbc0605105059ca354b068b0f79723fb2ac7358.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const express = require("express");
 const app = express();
 const cors = require('cors');
@@ -76,40 +75,3 @@ db.sequelize.sync().then(() => {
     });
 });
 
-=======
-var express = require('express')
-  , routes = require('./routes')
-  , path = require('path'),
-	fileUpload = require('express-fileupload'),
-	app = express(),
-	mysql      = require('mysql'),
-	bodyParser=require("body-parser");
-	
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'uploadimage'
-});
- 
-connection.connect();
- 
-global.db = connection;
- 
-// all environments
-app.set('port', process.env.PORT || 8080);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload());
- 
-// development only
- 
-app.get('/', routes.index);//call for main index page
-app.post('/', routes.index);//call for signup post 
-app.get('/profile/:id',routes.profile);
-//Middleware
-app.listen(8080)
->>>>>>> Stashed changes
