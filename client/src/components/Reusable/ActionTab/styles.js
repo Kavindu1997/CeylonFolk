5802120818ac4c4b8f99e973d1f073fc49fe8fb6
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-08-31 00:28:46 +0530

    Profile Card

--- client/src/components/Reusable/ActionTab/styles.js
+++ client/src/components/Reusable/ActionTab/styles.js
@@ -1,5 +1,6 @@
 import { makeStyles } from "@material-ui/core";
 import { blue } from "@material-ui/core/colors";
+import DrawerImg from '../../../images/drawer.jpg'
 
 export const useStyles = makeStyles((theme) => {
   console.log(theme.breakpoints);
@@ -13,6 +14,50 @@ export const useStyles = makeStyles((theme) => {
       backgroundColor: blue["A400"],
     },
 
+    profileImg: {
+      width: "100px",
+      height: "100px",
+      borderRadius: "50%",
+      color: "White",
+      backgroundColor:"#74b9ff",
+      fontSize:'3rem',
+      margin:'30px auto 10px auto'
+    },
+
+    profileName:{
+      fontWeight: 'bold',
+      display: 'block',
+      fontSize: '25px',
+      color: '#4e5052',
+      textTransform: 'capitalize',
+      textAlign:'center',
+      marginBottom: '5px',
+      lineHeight: '60px',      
+    },
+    divider: {
+      background:"#a4b0be",
+    },
+    drawer:{
+      backgroundImage: `url(${DrawerImg})`,
+      justifyContent:"center"
+    },
+    profileTitle:{
+      fontWeight: 'bold',
+      display: 'block',
+      fontSize: '18px',
+      color: 'black',
+      textTransform: 'capitalize',
+      textAlign:'center',
+      marginBottom: '0px',
+      lineHeight: '60px',      
+    },
+    profileDetail:{
+      fontWeight: 'bold',
+      display: 'block',
+      fontSize: '14px',
+      fontFamily: 'Nunito',
+      textAlign:'center',    
+    },
     //header
     wrapper: {
       width: "100%",
